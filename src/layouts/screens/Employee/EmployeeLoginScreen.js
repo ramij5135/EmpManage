739cfd6c755e5f4580b32072ca75aef241f51d93
raja <raja.swadesh631@gmail.com>
2022-11-04 17:40:20 +0530

    file change

---
+++ src/layouts/screens/Employee/EmployeeLoginScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Text,View,StyleSheet } from "react-native";
+const CancelOrder = () =>{
+    return(
+        <>
+        <View style={styles.container}>
+            <Text>hiiiii</Text>
+        </View>
+        </>
+    )
+}
+const styles=StyleSheet.create({
+    container:{
+
+    }
+})
+export default CancelOrder;     
