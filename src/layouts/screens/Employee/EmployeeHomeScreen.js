d68ca851125e213d5a321b9bbeae98fa9204d80e
ramij5135 <ramij.swadesh@gmail.com>
2022-11-08 15:04:40 +0530

    Emp:\ Employee home screen design complete

--- src/layouts/screens/Employee/EmployeeHomeScreen.js
+++ src/layouts/screens/Employee/EmployeeHomeScreen.js
@@ -1,14 +1,132 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
+
+const optionButton =[
+  {
+    id:0,
+    optionName:'In Time',
+    icon:'time-outline',
+    btnTitle:'In Time'
+  },
+  {
+    id:1,
+    optionName:'Out Time',
+    icon:'time-outline',
+    btnTitle:'Out Time'
+  },
+  {
+    id:2,
+    optionName:'Attendence',
+    icon:'calendar',
+    btnTitle:'Attendence'
+  },
+  {
+    id:3,
+    optionName:'My visit list',
+    icon:'list-outline',
+    btnTitle:'My visit list'
+  },
+  {
+    id:4,
+    optionName:'Your Task',
+    icon:'bicycle-outline',
+    btnTitle:'Your Task'
+  },
+  {
+    id:5,
+    optionName:'Visiting Report',
+    icon:'document-attach-outline',
+    btnTitle:'Visiting Report'
+  },
+  {
+    id:6,
+    optionName:'Add New Shop',
+    icon:'add-outline',
+    btnTitle:'Add New Shop'
+  },
+  {
+    id:7,
+    optionName:'My Location',
+    icon:'location-outline',
+    btnTitle:'My Location'
+  },
+  {
+    id:8,
+    optionName:'My Store',
+    icon:'home-outline',
+    btnTitle:'My Store'
+  },
+  {
+    id:9,
+    optionName:'Complaint',
+    icon:'notifications-off-outline',
+    btnTitle:'Complaint'
+  },
+]
 
 
 const HomeScreen = () => {
+  const [time, setTime] = useState('');
+  const [outTime, setOutTime] = useState('');
+  
+  const getInTime = () => {
+    const today = new Date();
+    const time = today.getHours() + ":" + today.getMinutes();
+    setTime(time)
+  }
+  const getOutTime = () => {
+    const today = new Date();
+    const time = today.getHours() + ":" + today.getMinutes();
+    setOutTime(time);
+  }
   return(
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.welcome}>Welcome</Text>
         <Text style={styles.welcomeName}>Ramij Dafadar</Text>
+      </View>
+      <View style={styles.wrapper}>
+        <View style={styles.square} />
+        <Image style={styles.profileImg} source={require('../../../assets/imgs/profile.jpg')} />
+        <Text style={styles.workTitle}>Sales Executive</Text>
+        <Text style={styles.workTime}>Office In-Out Time</Text>
+      </View>
+      <View style={{paddingHorizontal:20}}>
+        <View style={styles.option}>
+          {
+            optionButton.map((op) =>{
+              return(
+                <View key={op.id.toString()} style={styles.optionButton}>
+                  {
+                    op.btnTitle === 'In Time' ? 
+                    <>
+                      <Text style={styles.buttonTitle}>{op.optionName}</Text>
+                      <Text>{time}</Text>
+                      <View style={styles.button}>
+                        <Ionicons name='time-outline' size={15} color={'#fff'} />
+                        <Text onPress={getInTime} style={{color:'#fff', marginLeft:5}}>{op.btnTitle}</Text>
+                      </View>
+                    </> : op.btnTitle === 'Out Time' ? 
+                    <>
+                      <Text style={styles.buttonTitle}>{op.optionName}</Text>
+                      <Text>{outTime}</Text>
+                      <View style={styles.button}>
+                        <Ionicons name='time-outline' size={15} color={'#fff'} />
+                        <Text onPress={getOutTime} style={{color:'#fff', marginLeft:5}}>{op.btnTitle}</Text>
+                      </View>
+                    </> :
+                    <>
+                      <Ionicons name={op.icon} size={30} />
+                      <Text style={{fontFamily:'Poppins-Regular', fontSize:16}}>{op.optionName}</Text>
+                    </>
+                  }
+                </View>
+              )
+            })
+          }
+        </View>
       </View>
     </View>
   )
@@ -16,7 +134,6 @@ const HomeScreen = () => {
 
 const styles = StyleSheet.create({
   container:{
-    // padding:20,
     flex:1,
     backgroundColor:'rgba(130,209,209,0.4)',
   },
@@ -34,6 +151,83 @@ const styles = StyleSheet.create({
     fontFamily:'Poppins-SemiBold',
     fontSize:16,
     color:'green'
+  },
+  wrapper:{
+    backgroundColor:'rgba(255,255,255,0.4)',
+    borderBottomLeftRadius:100,
+    borderBottomRightRadius:100,
+    overflow:'hidden',
+    marginVertical:10,
+    paddingVertical:20,
+    alignItems:'center',
+  },
+  square:{
+    height:0,
+    width:0,
+    alignSelf:'center',
+    marginTop:-20,
+    top:0,
+    borderBottomWidth:20,
+    borderRightWidth:15,
+    borderLeftWidth:15,
+    borderBottomColor:'rgba(130,209,209,0.4)',
+    borderLeftColor:'transparent',
+    borderRightColor:'transparent',
+    borderTopColor:'transparent',
+    transform:[{rotate:'180deg'}],
+    marginBottom:10,
+    overflow:'hidden'
+  },
+  profileImg:{
+    height:70,
+    width:70,
+    borderRadius:50,
+    resizeMode:'contain',
+    alignSelf:'center',
+    marginBottom:20
+  },
+  workTitle:{
+    fontFamily:'Poppins-SemiBold',
+    fontSize:16,
+    color:'#2a2a2a',
+    letterSpacing:1
+  },
+  workTime:{
+    color:'#2a2a22',
+    fontFamily:'Poppins-Regular',
+    fontSize:16,
+    letterSpacing:1
+  },
+  option:{
+    flexDirection:'row',
+    flexWrap:'wrap',
+  },
+  optionButton:{
+    backgroundColor:'rgba(255,255,255,0.4)',
+    marginHorizontal:5,
+    // height:70,
+    paddingVertical:10,
+    width:Dimensions.get('window').width*0.42,
+    alignItems:'center',
+    justifyContent:'center',
+    borderRadius:15,
+    overflow:'hidden',
+    marginVertical:5
+  },
+  buttonTitle:{
+    fontFamily:'Poppins-Regular',
+    fontSize:16,
+    color:'#2a2a22'
+  },
+  button:{
+    flexDirection:'row', 
+    alignItems:'center', 
+    backgroundColor:'blue', 
+    width:120,
+    paddingVertical:5, 
+    justifyContent:'center',
+    borderRadius:10,
+    marginTop:10
   }
 });
 
