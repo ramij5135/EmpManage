78e46aa60c53750f001c19852cba7cf2ce38a674
ramij5135 <ramij.swadesh@gmail.com>
2022-11-15 17:26:48 +0530

    profile screen & new shop screen add

--- src/layouts/screens/Employee/NewShopScreen.js
+++ src/layouts/screens/Employee/NewShopScreen.js
@@ -1,12 +1,32 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image, Text, ScrollView } from "react-native";
 import { COLORS } from "../../../utils/globalStyles";
 import Header from "../../components/header";
+import Inputs from "../../components/inputs";
+import FullButton from "../../components/fullButton";
 
 const NewShopScreen = () => {
     return(
         <View style={styles.container}>
-
+            <Header title={'New Shop'} />
+            <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={styles.header}>
+                <Image style={styles.imgIcon} source={require('../../../assets/imgs/build.png')} />
+                <Text style={styles.attendence}>Add New Shop</Text>
+            </View>
+            <View style={styles.inputSection}>
+                <Inputs title={'Shop Images'} iconName={'add'} />
+                <Inputs title={'Shop Name'} />
+                <Inputs title={'Owner Name'} />
+                <Inputs title={'Email'} />
+                <Inputs title={'Contact Number'} />
+                <Inputs title={'Address'} />
+                <Inputs title={'Description'} />
+            </View>
+            <View style={{paddingHorizontal:10, paddingVertical:20}}>
+                <FullButton btnTitle={'Add New Shop'} />
+            </View>
+            </ScrollView>
         </View>
     )
 }
@@ -15,6 +35,25 @@ const styles = StyleSheet.create({
     container:{
         flex:1,
         backgroundColor:COLORS.primary
+    },
+    header:{
+        backgroundColor:COLORS.white,
+        flexDirection:'row',
+        justifyContent:'space-between',
+        paddingHorizontal:20,
+        alignItems:'center'
+    },
+    imgIcon:{
+        height:50,
+        width:50,
+    },
+    attendence:{
+        fontFamily:'Poppins-SemiBold',
+        fontSize:16,
+        color:COLORS.black
+    },
+    inputSection:{
+        paddingHorizontal:20
     }
 });
 
--- src/layouts/screens/Employee/ProfileScreen.js
+++ src/layouts/screens/Employee/ProfileScreen.js
@@ -1,16 +1,93 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { COLORS } from "../../../utils/globalStyles";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const ProfileScreen = () => {
     return(
-        <View>
-
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.welcome}>Welcome</Text>
+                <Text style={styles.welcomeName}>Ramij Dafadar</Text>
+            </View>
+            <View style={styles.wrapper}>
+                <View style={styles.square} />
+                <Image style={styles.profileImg} source={require('../../../assets/imgs/profile.jpg')} />
+                <Text style={styles.workTitle}>Sales Executive</Text>
+            </View>
+            <View style={{paddingHorizontal:20, alignItems:'center'}}>
+                <View style={{flexDirection:'row', alignItems:'center'}}>
+                    <Ionicons name="person" size={25} />
+                    <View>
+                        <Text>Employee Id</Text>
+                        <Text>3</Text>
+                    </View>
+                </View>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    container:{
+        flex:1,
+        backgroundColor:COLORS.primary
+    },
+    header:{
+        backgroundColor:COLORS.white,
+        height:60,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    welcome:{
+        fontFamily:'Poppins-Regular',
+        fontSize:16
+    }, 
+    welcomeName:{
+        fontFamily:'Poppins-SemiBold',
+        fontSize:16,
+        color:'green'
+    },
+    wrapper:{
+        backgroundColor:COLORS.white,
+        borderBottomLeftRadius:100,
+        borderBottomRightRadius:100,
+        overflow:'hidden',
+        marginVertical:10,
+        paddingVertical:20,
+        alignItems:'center',
+    },
+    square:{
+        height:0,
+        width:0,
+        alignSelf:'center',
+        marginTop:-20,
+        top:0,
+        borderBottomWidth:20,
+        borderRightWidth:15,
+        borderLeftWidth:15,
+        borderBottomColor:'rgba(130,209,209,0.4)',
+        borderLeftColor:'transparent',
+        borderRightColor:'transparent',
+        borderTopColor:'transparent',
+        transform:[{rotate:'180deg'}],
+        marginBottom:10,
+        overflow:'hidden'
+    },
+    profileImg:{
+        height:70,
+        width:70,
+        borderRadius:50,
+        resizeMode:'contain',
+        alignSelf:'center',
+        marginBottom:20
+    },
+    workTitle:{
+        fontFamily:'Poppins-SemiBold',
+        fontSize:16,
+        color:'#2a2a2a',
+        letterSpacing:1
+    },
 });
 
 export default ProfileScreen;
