c5ffddd1a1a9b90500ad53fb23308681e2491023
raja <raja.swadesh631@gmail.com>
2022-11-16 09:03:34 +0530

    work update admin section

--- src/layouts/screens/Admin/VichelScreen.js
+++ src/layouts/screens/Admin/VichelScreen.js
@@ -1,72 +1,97 @@
 import React, { useState } from 'react'
 import { Button, view, Text, StyleSheet, TouchableOpacity, View } from 'react-native'
 import DeviceInfo from 'react-native-device-info';
+import DatePicker from 'react-native-datepicker'
 const VicheleScreen = () => {
-    const [deviceId, setdeviceId] = useState()
-    const getDvicedId = () => {
-       DeviceInfo.getUniqueId().then((res)=>{
-            console.log('res',res);
-            setdeviceId(res)
-        })
-        // console.log('uniqueId=======', uniqueId);
-        // setdeviceId(uniqueId)
-    }
-   
+  const [deviceId, setdeviceId] = useState()
+  const getDvicedId = () => {
+    DeviceInfo.getUniqueId().then((res) => {
+      console.log('res', res);
+      setdeviceId(res)
+    })
+    // console.log('uniqueId=======', uniqueId);
+    // setdeviceId(uniqueId)
+  }
 
-    const orderId = () => {
-        var S4 = () => {
-          return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-        };
-setdeviceId(S4)
-        return (
-          S4() +
-          S4() +
-          "-" +
-          S4() +
-          "-" +
-          S4() +
-          "-" +
-          S4() +
-          "-" +
-          S4() +
-          S4() +
-          S4()
-        );
-      };
-    //   const random=()=>{
 
-    //     let x = Math.floor((Math.random()) + 1);
-    //     return (
-
-    //         <>
-    //         </>
-
-    //     )
-        
-    //   }
-      const [count,setCount]=useState(1)
-      
+  const orderId = () => {
+    var S4 = () => {
+      return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    };
+    setdeviceId(S4)
     return (
-        <>
-            
-       
-                   { count<10?<Text>SW_00{count}</Text>
-                   :
-                count>=10 && count<100?<Text>SW_0{count}</Text>:
-                count>=100?<Text>SW_{count}</Text>:null
-                
-                }
-                
+      S4() +
+      S4() +
+      "-" +
+      S4() +
+      "-" +
+      S4() +
+      "-" +
+      S4() +
+      "-" +
+      S4() +
+      S4() +
+      S4()
+    );
+  };
+  //   const random=()=>{
 
-            <TouchableOpacity
-                onPress={() => setCount(count + 1)
-                }
-            >
+  //     let x = Math.floor((Math.random()) + 1);
+  //     return (
 
-                <Text>click</Text>
-            </TouchableOpacity>
-        </>
-    )
+  //         <>
+  //         </>
+
+  //     )
+
+  //   }
+  const [count, setCount] = useState(1)
+  const [date,setDate] = useState()
+  return (
+    <>
+
+
+<DatePicker
+        style={{width: 200}}
+        date={date}
+        mode="date"
+        placeholder="select date"
+        format="YYYY-MM-DD"
+        minDate="2016-05-01"
+        maxDate="2016-06-01"
+        confirmBtnText="Confirm"
+        cancelBtnText="Cancel"
+        customStyles={{
+          dateIcon: {
+            position: 'absolute',
+            left: 0,
+            top: 4,
+            marginLeft: 0
+          },
+          dateInput: {
+            marginLeft: 36
+          }
+          // ... You can check the source to find the other keys.
+        }}
+        onDateChange={(date) => {setDate(date)}}
+      />
+      {/* {count < 10 ? <Text>SW_00{count}</Text>
+        :
+        count >= 10 && count < 100 ? <Text>SW_0{count}</Text> :
+          count >= 100 ? <Text>SW_{count}</Text> : null
+
+      }
+
+
+      <TouchableOpacity
+        onPress={() => setCount(count + 1)
+        }
+      >
+
+        <Text>click</Text>
+      </TouchableOpacity> */}
+    </>
+  )
 }
 // const styles=StyleSheet.create({
 //     container:{
